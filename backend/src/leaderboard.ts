6a6b11f320665d17be585a1cd1794bdff48583d8
zurdooo <a.ramos37@Hotmail.com>
2025-11-30 21:10:07 -0500

    feat: Update leaderboard data structure to use LeaderboardEntry interface

--- backend/src/leaderboard.ts
+++ backend/src/leaderboard.ts
@@ -1,6 +1,10 @@
-import { Request, Response } from 'express';
-import { query } from './db';
-import { CodeStats } from './types/leaderboard';
+import { Request, Response } from "express";
+import { query } from "./db";
+import { LeaderboardEntry } from "./types/leaderboard";
+
+/*
+User requests leaderboard data, requests two options, overall leaderboard or leaderboard for a specific day, for now language is ignored
+*/
 
 // GET /api/leaderboard
 export const getLeaderboard = async (req: Request, res: Response) => {
@@ -8,7 +12,7 @@ export const getLeaderboard = async (req: Request, res: Response) => {
     // TODO: Implement fetching stats from DB
     // const { day, language } = req.query;
     // const result = await query('SELECT * FROM leaderboard ...');
-    
+
     res.json({ message: "Leaderboard data will go here" });
   } catch (err) {
     console.error(err);
@@ -19,8 +23,8 @@ export const getLeaderboard = async (req: Request, res: Response) => {
 // POST /api/submit-stats
 export const submitStats = async (req: Request, res: Response) => {
   try {
-    const stats: CodeStats = req.body;
-    
+    const stats: LeaderboardEntry = req.body;
+
     // TODO: Validate input
     // TODO: Insert into DB
     // await query('INSERT INTO leaderboard ...', [stats.userId, stats.day...]);
